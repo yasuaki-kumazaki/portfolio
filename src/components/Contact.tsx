5f1476f91935292fd0e8f240ce5724359a08366e
yasuaki-kumazaki <yasuaki.kumazaki@haw-hamburg.de>
2024-06-16 23:34:58 +0200

    fixing a bug

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,12 +5,12 @@ const Contact = () => {
   return (
     <div>
       <h2>
-        Contact me! Have any questions? Fill out the form and I'll get you a
+        {/* Contact me! Have any questions? Fill out the form and I'll get you a
         response soon! Feel free to ask / inquire about anything (school,
         basketball, internships, music, etc). Hoping to be a resource and be of
         help in your journey in any way possible. Similarly, please use this
         form or email directly me if your message regards a NIL opportunity.
-        kaylapadilla.business@gmail.com
+        kaylapadilla.business@gmail.com */}
       </h2>
       <ul>
         <li>
